f4bf130ae6026e3e3cbf6838086278f3a7dfdddc
islamosman2541998 <islamosman2541998@gmail.com>
2026-02-25 12:45:47 +0200

    Update index.blade.php

--- resources/views/livewire/site/payments/index.blade.php
+++ resources/views/livewire/site/payments/index.blade.php
@@ -1,59 +1,63 @@
 <div id="payment-methods">
     <div class="TotalCard">
-        @if(@auth('account')->user()?->types->where('type', 'donor')->first() == null)
+        @if (@auth('account')->user()?->types->where('type', 'donor')->first() == null)
             <span class="text-danger">من فضلك قم بتسجيل الدخول لاستكمال عملية الدفع </span>
         @else
-
-        <div class="payment-methods">
-            <div class="img-container">
-                @if ($visaStatus)
-                <button type="button" wire:click="SelectPayment('visa')" id="visa" class="button payment-items  @if($paymentMethod == 'visa') active @endif">
-                    <span>
-                        <img src="{{ site_path('img/pay-4.png') }}" alt="" />
-                    </span>
-                </button>
-                @endif
-                @if ($applePayStatus && ($iphone || $safari))
-                    <button type="button" wire:click="SelectPayment('applePay')" id="apple-pay" class="button payment-items @if($paymentMethod == 'applePay') active @endif">
-                        <span>
-                            <img src="{{ site_path('img/pay-2.png') }}" alt="" />
-                        </span>
-                    </button>
-
-                    @if(!config("app.TEST_MODE"))
-                        <input type="hidden" id="SHARequestPhrase" value="{{config("payfort.SHARequestPhrase")}}">
-                        @else
-                        <input type="hidden" id="SHARequestPhrase" value="96o0CiKlNkSJO7/OJH8ALl$+">
+            <div class="payment-methods">
+                <div class="img-container">
+                    @if ($visaStatus)
+                        <button type="button" wire:click="SelectPayment('visa')" id="visa"
+                            class="button payment-items  @if ($paymentMethod == 'visa') active @endif">
+                            <span>
+                                <img src="{{ site_path('img/pay-4.png') }}" alt="" />
+                            </span>
+                        </button>
                     @endif
-                @endif
-                @if ($banktransferStatus)
-                <button type="button" wire:click="SelectPayment('bankTransfer')" id="bank" class="button payment-items @if($paymentMethod == 'bankTransfer') active @endif">
-                    <span>
-                        <img src="{{ site_path('img/pay-1.png') }}" alt="" />
-                    </span>
-                </button>
-                @endif
+                    @if ($applePayStatus && ($iphone || $safari))
+                        <button type="button" wire:click="SelectPayment('applePay')" id="apple-pay"
+                            class="button payment-items @if ($paymentMethod == 'applePay') active @endif">
+                            <span>
+                                <img src="{{ site_path('img/pay-2.png') }}" alt="" />
+                            </span>
+                        </button>
+
+                        @if (!config('app.TEST_MODE'))
+                            <input type="hidden" id="SHARequestPhrase"
+                                value="{{ config('payfort.SHARequestPhrase') }}">
+                        @else
+                            <input type="hidden" id="SHARequestPhrase" value="96o0CiKlNkSJO7/OJH8ALl$+">
+                        @endif
+                    @endif
+                    @if ($banktransferStatus)
+                        <button type="button" wire:click="SelectPayment('bankTransfer')" id="bank"
+                            class="button payment-items @if ($paymentMethod == 'bankTransfer') active @endif">
+                            <span>
+                                <img src="{{ site_path('img/pay-1.png') }}" alt="" />
+                            </span>
+                        </button>
+                    @endif
+                </div>
+
+                <div class="label">
+
+                    <p>
+                        وسيلة الدفع
+                    </p>
+                </div>
+
             </div>
 
-            <div class="label">
+           <div @if($paymentMethod != "visa" || !$visaStatus) style="display:none" @endif>
+    @livewire('site.payments.visa')
+</div>
 
-              <p>
-                وسيلة الدفع
-              </p>
-            </div>
+<div @if($paymentMethod != "applePay" || !$applePayStatus) style="display:none" @endif>
+    @livewire('site.payments.apple-pay')
+</div>
 
-          </div>
-
-            <!-- visa -->
-            @if($paymentMethod == "visa" && $visaStatus)
-                @livewire('site.payments.visa')
-            <!-- apple-pay-tab -->
-            @elseif($paymentMethod == "applePay" && $applePayStatus)
-                @livewire('site.payments.apple-pay')
-            <!-- transfer-pay-tab -->
-            @elseif($paymentMethod == "bankTransfer" && $banktransferStatus)
-                @livewire('site.payments.bank-transfer')
-            @endif
+<div @if($paymentMethod != "bankTransfer" || !$banktransferStatus) style="display:none" @endif>
+    @livewire('site.payments.bank-transfer')
+</div>
 
         @endif
     </div>
@@ -69,7 +73,8 @@
 
         function CalculateSignature() {
             // const requestShaPhrase = "96o0CiKlNkSJO7/OJH8ALl$+"; // Set your request SHA phrase here.
-            const requestShaPhrase = document.getElementById("SHARequestPhrase").value; // "737LIJbY2e1b5sTd0.8iPE+_"; // Set your request SHA phrase here.
+            const requestShaPhrase = document.getElementById("SHARequestPhrase")
+            .value; // "737LIJbY2e1b5sTd0.8iPE+_"; // Set your request SHA phrase here.
 
             let signatureString = requestShaPhrase;
 
@@ -104,6 +109,5 @@
             // Submit the form
             document.getElementById("paymentForm").submit();
         }
-
     </script>
 </div>
