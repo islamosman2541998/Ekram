6c34f5200783d332fc0cd4ec1545a02e173701b4
islamosman2541998 <islamosman2541998@gmail.com>
2026-03-12 15:46:25 +0200

    Update Register.php

--- app/Http/Livewire/Site/Auth/Register.php
+++ app/Http/Livewire/Site/Auth/Register.php
@@ -24,7 +24,7 @@ class Register extends Component
     public $testMobiles = [
         "966597767751",
         "966567296308",
-        "966561611117",
+       // "966561611117",
         "966540265614",
     ];
 
