1ff4f4fddfda5090895afbc525e44611d7c26c15
islamosman2541998 <islamosman2541998@gmail.com>
2026-04-23 11:14:26 +0200

    Update moyasar-payment.blade.php

--- resources/views/site/pages/checkout/moyasar-payment.blade.php
+++ resources/views/site/pages/checkout/moyasar-payment.blade.php
@@ -1,53 +1,60 @@
-
-
 @extends('site.payment-layout')
 
 @section('title', 'إتمام الدفع')
 
 @section('content')
-<div class="container" style="max-width:600px; margin:40px auto; padding:20px;">
-    
-    <div style="text-align:center; margin-bottom:30px;">
-        <h4>إتمام الدفع</h4>
-        <div style="font-size:28px; font-weight:bold; color:#28a745;">
-            {{ number_format($order->total, 2) }} <small>ر.س</small>
+    <div class="container" style="max-width:600px; margin:40px auto; padding:20px;">
+
+        <div style="text-align:center; margin-bottom:30px;">
+            <h4>إتمام الدفع</h4>
+            <div style="font-size:28px; font-weight:bold; color:#28a745;">
+                {{ number_format($order->total, 2) }} <small>ر.س</small>
+            </div>
+            <div style="color:#888; font-size:14px;">
+                طلب رقم #{{ $order->identifier }}
+            </div>
         </div>
-        <div style="color:#888; font-size:14px;">
-            طلب رقم #{{ $order->identifier }}
-        </div>
+
+        <!-- Moyasar Payment Form -->
+        <div class="mysr-form"></div>
+
     </div>
-
-    <!-- Moyasar Payment Form -->
-    <div class="mysr-form"></div>
-
-</div>
 @endsection
 
 @push('js')
-<script src="https://cdn.moyasar.com/mpf/1.7.3/moyasar.js"></script>
-<script>
-    Moyasar.init({
-        element: '.mysr-form',
-        amount: {{ (int) $amount }},
-        currency: 'SAR',
-        description: '{{ addslashes($description) }}',
-        publishable_api_key: '{{ $publishableKey }}',
-        callback_url: '{{ url("moyasar-callback") }}?order_id={{ $order->id }}',
-        language: 'ar',
-        methods: {!! json_encode($methods) !!},
-        supported_networks: ['visa', 'mastercard', 'mada'],
-        metadata: {
-            order_id: '{{ $order->id }}',
-            order_identifier: '{{ $order->identifier }}',
-        },
-        apple_pay: {
-            country: 'SA',
-            label: 'جمعية إكرام المسنين',
-            validate_merchant_url: 'https://api.moyasar.com/v1/applepay/initiate'
-        },
+    <script src="https://cdn.moyasar.com/mpf/1.7.3/moyasar.js"></script>
+    <script>
+        Moyasar.init({
+            element: '.mysr-form',
+            amount: {{ (int) $amount }},
+            currency: 'SAR',
+            description: '{{ addslashes($description) }}',
+            publishable_api_key: '{{ $publishableKey }}',
+            callback_url: '{{ url('moyasar-callback') }}?order_id={{ $order->id }}',
+            language: 'ar',
+            methods: {!! json_encode($methods) !!},
+            supported_networks: ['visa', 'mastercard', 'mada'],
+            metadata: {
+                order_id: '{{ $order->id }}',
+                order_identifier: '{{ $order->identifier }}',
+            },
+            apple_pay: {
+                country: 'SA',
+                label: 'جمعية إكرام المسنين',
+                validate_merchant_url: 'https://api.moyasar.com/v1/applepay/initiate'
+            },
             on_failure: function(error) {
-        alert(JSON.stringify(error));
-    }
+                console.log('type:', typeof error);
+                console.log('keys:', Object.keys(error));
+                console.log('str:', JSON.stringify(error));
+
+                // اعرض كل حاجة
+                let msg = '';
+                for (let k in error) {
+                    msg += k + ': ' + error[k] + '\n';
+                }
+                alert(msg || 'empty error - card cancelled by Apple');
+            }
         });
-</script>
+    </script>
 @endpush
