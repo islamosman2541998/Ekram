5489afb2ee8be47b44b4202590a9b08607ce4f81
islamosman2541998 <islamosman2541998@gmail.com>
2026-05-05 16:56:52 +0300

    Update header.blade.php

--- resources/views/components/site/layouts/header.blade.php
+++ resources/views/components/site/layouts/header.blade.php
@@ -18,12 +18,23 @@
     <meta property="og:description" content=" " />
     <meta property="og:url" content="{{ url()->current() }}" />
     <meta name="og:image" content="{{ asset($settings->getItem('logo')) }}">
+    <!-- OpenGraph -->
+    <meta property="og:title" content=" {{ $settings->getItem('site_name') }}  | @yield('title', $settings->getItem('meta_title_' . $current_lang))" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content=" {{ $settings->getItem('meta_description_' . $current_lang) }}" />
+    <meta property="og:url" content="{{ url()->current() }}" />
+    <meta property="og:image" content="{{ asset('img/social-preview.png') }}">
+    <meta property="og:image:secure_url" content="{{ asset('img/social-preview.png') }}">
+    <meta property="og:image:type" content="image/jpeg">
+    <meta property="og:image:width" content="1200">
+    <meta property="og:image:height" content="630">
 
+    <meta name="twitter:image" content="{{ asset('img/social-preview.png') }}">
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image" />
-    <meta property="twitter:url" content />
-    <meta property="twitter:title" content />
-    <meta property="twitter:description" content=" " />
+    <meta property="twitter:url" content="{{ url()->current() }}" />
+    <meta property="twitter:title" content=" {{ $settings->getItem('site_name') }}  | @yield('title', $settings->getItem('meta_title_' . $current_lang))" />
+    <meta property="twitter:description" content=" {{ $settings->getItem('meta_description_' . $current_lang) }}" />
     <meta property="twitter:image" content />
 
 
